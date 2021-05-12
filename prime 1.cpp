5fe52c32fa1db7176f2c14c65db4c8ff3913e9c1
satvika39 <69497952+satvika39@users.noreply.github.com>
2021-05-11 23:53:54 -0700

    Add files via upload

---
+++ prime 1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i,fact=1;
+	cin>>n;
+	cout<<"enter the number";
+	for(i=2;i<sqrt(n-1);i++)
+	{
+		if(n%i==0)
+		{
+			fact==0;
+			cout<<" it is not a prime";
+			break;
+			
+		}
+		else
+	     cout<<" it is a prime";
+	
+	}
+	return 0;
+
+}
